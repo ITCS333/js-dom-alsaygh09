9cf09a50690157529f6ca2705c735712b0505c54
alsaygh09 <alsaygh2005@gmail.com>
2026-03-30 20:11:49 +0300

    .

--- index.js
+++ index.js
@@ -2,36 +2,45 @@
  * Selects the element with the id 'main-heading' and changes its text content to 'DOM Manipulation Challenge'.
  */
 function changeHeadingText() {
-  // TODO: Implement this function
+  let head = document.getElementById("main-heading");
+  head.textContent = "DOM Manipulation Challenge";
 }
 
 /**
  * Selects the element with the id 'box-to-modify' and changes its background color to 'lightblue'.
  */
 function changeBoxColor() {
-  // TODO: Implement this function
+  let boxs = document.getElementById("box-to-modify");
+  boxs.style.background = "lightblue";
 }
 
 /**
  * Creates a new <li> element, sets its text content to 'New Item', and appends it to the <ul> with the id 'item-list'.
  */
 function addNewItem() {
-  // TODO: Implement this function
+  let newli = document.createElement("li");
+  newli.textContent = "New Item";
+
+  let ullist = document.getElementById("item-list");
+  ullist.appendChild(newli);
 }
 
 /**
  * Selects the paragraph with the class 'content-para' and adds the class 'highlight' to it.
  */
 function highlightParagraph() {
-  // TODO: Implement this function
+  let p = document.querySelector(".content-para");
+  p.classList.add("highlight");
 }
 
 /**
  * Selects the element with the id 'to-be-removed' and removes it from the DOM.
  */
 function removeElement() {
-  // TODO: Implement this function
+  let element = document.getElementById("to-be-removed");
+  element.remove();
 }
+
 
 
 // Do not edit the lines below.
